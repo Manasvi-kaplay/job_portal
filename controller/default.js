434d2c4995ac294a34115f8899d68db92a37878e
manasvi kaplay <manasvi111.kaplay@gmail.com>
2019-12-31 16:56:29 +0530

    31 dec

--- controller/default.js
+++ controller/default.js
@@ -1,6 +1,6 @@
 var express=require("express");
 var router=express.Router();
-router.use("/", require("./login"));
+// router.use("/", require("./login"));
 router.use("/reg_employer",require("./reg_employer"))
 //router.use("/reg_jobseeker",require("./reg_jobseeker"))
 function backdoor(req, res, next)
